abd587d5330c5e335f8457f1a39de58af0a2a278
A. Karimov <ainureg@users.noreply.github.com>
2015-08-18 11:58:17 +0300

    Update 1.c

--- 1.c
+++ 1.c
@@ -7,8 +7,7 @@ double tol=0.000001;
 int k=1000;
 
 int i;
-double S;
-double Q;
+double S,Q;
 double begin, end, new;
 
 int myrank, size;
@@ -19,7 +18,7 @@ MPI_Comm_size(MPI_COMM_WORLD, &size);
 double h=1/(double)(k-1)/(double)size;
 
 MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
-MPI_Request Request;
+
 MPI_Status Status;
 
 begin= myrank/(double)(size);
@@ -32,7 +31,7 @@ for (i=0;i<k-1;i++){
 MPI_Reduce(&S,&Q, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
 
 if (myrank==0){
-	printf("%f\n",Q);
+	printf("%10.10f\n",Q);
 }
 
 MPI_Finalize();
