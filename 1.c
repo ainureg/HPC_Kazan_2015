09c5c36c3dca7dd6037c1c5d17487511bb8024f5
A. Karimov <ainureg@users.noreply.github.com>
2015-08-18 11:17:43 +0300

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,40 @@
+#include "mpi.h"
+#include "stdio.h"
+#include "stdlib.h"
+
+int main(int argc, char *argv[]){
+double tol=0.000001;
+int k=1000;
+
+int i;
+double S;
+double Q;
+double begin, end, new;
+
+int myrank, size;
+
+MPI_Init(&argc, &argv);
+MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+double h=1/(double)(k-1)/(double)size;
+
+MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
+MPI_Request Request;
+MPI_Status Status;
+
+begin= myrank/(double)(size);
+for (i=0;i<k-1;i++){
+	end=begin+h;
+	new=(1/(1+begin*begin) +1/(1+end*end))*2*h;
+	S=S+new;
+	begin=end;
+	}
+MPI_Reduce(&S,&Q, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+
+if (myrank==0){
+	printf("%f\n",Q);
+}
+
+MPI_Finalize();
+return 0;
+}
